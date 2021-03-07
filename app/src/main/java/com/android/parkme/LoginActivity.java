13fd01ebc37acd235cb7cd71c5f8d7221afb052b
Shivam Sharma <shivam20121@iiitd.ac.in>
2021-03-08 00:19:57 +0530

    navigation bar, fragments

--- app/src/main/java/com/android/parkme/LoginActivity.java
+++ app/src/main/java/com/android/parkme/LoginActivity.java
@@ -2,7 +2,6 @@ package com.android.parkme;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,7 +22,7 @@ public class LoginActivity extends AppCompatActivity {
 
     // on successful login application navigates to the home screen
     private void onSuccess() {
-        Intent intent = new Intent(getApplicationContext(),HomeActivity.class);
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_TASK_ON_HOME);
         startActivity(intent);
         // we don't want to display login screen again on pressing the back button
---
+++ app/src/main/java/com/android/parkme/MainActivity.java
@@ -0,0 +1,36 @@
+package com.android.parkme;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView navbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        navbar = findViewById(R.id.bottomNavigationView);
+        navbar.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.profile:
+                    setCurrentFragment(new PersonalDetailsFragment());
+                    break;
+                case R.id.home:
+                    setCurrentFragment(new HomeFragment());
+                    break;
+            }
+            return true;
+        });
+        setCurrentFragment(new HomeFragment());
+    }
+
+    private void setCurrentFragment(Fragment fragment) {
+            getSupportFragmentManager().beginTransaction().
+        replace(R.id.flFragment,fragment).
+        commit();
+    }
+}
