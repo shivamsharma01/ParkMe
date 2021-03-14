1787175d253ad6992613b2f3a72f28b9b76ba6a1
Shradha Sabhlok <shradha20069@iiitd.ac.in>
2021-03-14 17:31:42 +0530

    Implemented Camera Integration and Reset Form Functionality
    Pending Items:
    1. Send button Functionality
    2. Resetting the QueryType dropdown value

--- app/src/main/java/com/android/parkme/MainActivity.java
+++ app/src/main/java/com/android/parkme/MainActivity.java
@@ -4,10 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
--- app/src/main/java/com/android/parkme/RaiseQueryFragment.java
+++ app/src/main/java/com/android/parkme/RaiseQueryFragment.java
@@ -1,23 +1,33 @@
 package com.android.parkme;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
+import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class RaiseQueryFragment extends Fragment {
     private Spinner queryTypeDropdown;
-    private EditText dateText;
+    private EditText dateText, messageText, vehicleNumber;
     private ImageView clickedImage;
+    private FloatingActionButton addImage;
+    public static final int CAMERA_REQUEST=9999;
+    private Button resetBtn;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -40,11 +50,44 @@ public class RaiseQueryFragment extends Fragment {
         queryTypeAdaptor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         queryTypeDropdown.setAdapter(queryTypeAdaptor);
 
+        messageText = getActivity().findViewById(R.id.message_text);
+        vehicleNumber = getActivity().findViewById(R.id.number_value);
+
         dateText = getActivity().findViewById(R.id.date_value);
         SimpleDateFormat sdf = new SimpleDateFormat( "dd/MM/yyyy" );
         dateText.setText(sdf.format( new Date()));
 
-        clickedImage = getActivity().findViewById(R.id.clicked_image);
-    }
+        addImage = getActivity().findViewById(R.id.add_image_button);
+        addImage.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                OpenCamera(v);
+            }
+        });
 
+        clickedImage = getActivity().findViewById(R.id.clicked_image);
+
+        resetBtn = getActivity().findViewById(R.id.reset_button);
+        resetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                messageText.setText("");
+                vehicleNumber.setText("");
+                clickedImage.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+    public void OpenCamera(View view) {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==CAMERA_REQUEST){
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            Bitmap resizedBitmap = Bitmap.createScaledBitmap(
+                    bitmap, 420, 60, false);
+            clickedImage.setImageBitmap(resizedBitmap);
+            clickedImage.setVisibility(View.VISIBLE);
+        }
+    }
 }
