22c6c6fbdd27bdbb12ccf898e77c476d48ebb989
official-akhil <akhil20107@iiitd.ac.in>
2021-03-15 14:10:43 +0530

    XML changes

--- app/src/main/java/com/android/parkme/LoginActivity.java
+++ app/src/main/java/com/android/parkme/LoginActivity.java
@@ -9,30 +9,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
     Button login, loginUsingPhone;
-    TextView forgotpassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         login = findViewById(R.id.login_button);
-        forgotpassword = findViewById(R.id.fpasword_text);
         loginUsingPhone = findViewById(R.id.login_sign_in_with_the_phone_number);
         login.setOnClickListener(v -> {
             onSuccess();
         });
-        forgotpassword.setOnClickListener(v->{
-            fpassword();
-        });
     }
-
-    private void fpassword()
-    {
-        Intent intent = new Intent(getApplicationContext(), ForgotPassword.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_TASK_ON_HOME);
-        startActivity(intent);
-    }
-    // on successful login application navigates to the home screen
     private void onSuccess() {
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_TASK_ON_HOME);
