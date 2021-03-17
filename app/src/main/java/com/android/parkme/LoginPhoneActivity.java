41255f7623063838b0b5fbc552b9c3b290538642
Shivam Sharma <shivam20121@iiitd.ac.in>
2021-03-18 03:32:09 +0530

    login phone

--- app/src/main/java/com/android/parkme/LoginPhoneActivity.java
+++ app/src/main/java/com/android/parkme/LoginPhoneActivity.java
@@ -76,7 +76,6 @@ public class LoginPhoneActivity extends AppCompatActivity {
     private void handleError(VolleyError error) {
         try {
             String responseBody = new String(error.networkResponse.data, "utf-8");
-            System.out.println(responseBody);
             JSONObject data = new JSONObject(responseBody);
             int status = data.getInt("status");
             String errorString = data.getString("trace");
