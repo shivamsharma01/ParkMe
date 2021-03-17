f569cd00dff84478b17ff419abe4c9c68212c0eb
Shivam Sharma <shivam20121@iiitd.ac.in>
2021-03-18 04:04:29 +0530

    home page name

--- app/src/main/java/com/android/parkme/HomeFragment.java
+++ app/src/main/java/com/android/parkme/HomeFragment.java
@@ -1,10 +1,13 @@
 package com.android.parkme;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -12,6 +15,11 @@ import androidx.fragment.app.FragmentTransaction;
 
 public class HomeFragment extends Fragment {
     ImageView raiseQueryOptions, profilePic, settings;
+    private SharedPreferences sharedpreferences;
+    private static final String MyPREFERENCES = "ParkMe";
+    private static final String sessionKey = "sessionKey";
+    private static final String name = "fullname";
+    TextView textView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,9 +30,12 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+        sharedpreferences = getActivity().getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
         raiseQueryOptions = view.findViewById(R.id.raise_query_option);
         raiseQueryOptions.setOnClickListener(v -> openFragment(new RaiseQueryFragment()));
         profilePic = view.findViewById(R.id.imageView);
+        textView = view.findViewById(R.id.textView);
+        textView.setText(String.format(getActivity().getResources().getString(R.string.greet).toString(), sharedpreferences.getString(name, "")));
         profilePic.setOnClickListener(v -> openFragment(new PersonalDetailsFragment()));
         settings = view.findViewById(R.id.settings_button);
         settings.setOnClickListener(v -> openFragment(new SettingsFragment()));
