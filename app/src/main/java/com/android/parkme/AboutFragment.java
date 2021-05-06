552264632b431ae550d4a7fecb8bc4cb4d4bdffb
Shivam Sharma <shivam20121@iiitd.ac.in>
2021-05-06 13:19:04 +0530

    resources

--- app/src/main/java/com/android/parkme/AboutFragment.java
+++ app/src/main/java/com/android/parkme/AboutFragment.java
@@ -1,7 +1,24 @@
 package com.android.parkme;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
 
 public class AboutFragment extends Fragment {
     private static final String TAG = "AboutFragment";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_about, container, false);
+    }
+
 }
